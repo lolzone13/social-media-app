15f26667657cdfd1c1d7843f1c7db62c44e4f709
lolzone13 <mohitscience123@gmail.com>
2022-07-01 01:20:31 +0530

    Created login/sign-up form

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,39 +1,37 @@
 import Link from "next/link";
-
+import { useState } from "react";
 
 
 export default function login() {
 
+    const [loginData, setLoginData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(loginData);
+    }
 
     return (
         <>
         <div className="p-4 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-            <form className="space-y-6" action="#">
+            <form onSubmit={handleSubmit} className="space-y-6" >
                 <h5 className="text-xl font-medium text-gray-900 dark:text-white">Sign in</h5>
                 <div>
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
-                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="" />
+                    <input onChange={(e) => setLoginData({email: e.target.value, password: loginData.password})} type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="name@company.com" required="" />
                 </div>
                 <div>
                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
-                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
-                </div>
-                <div className="flex items-start">
-                    <div className="flex items-start">
-                        <div className="flex items-center h-5">
-                            <input id="remember" type="checkbox" value="" className="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="" />
-                        </div>
-                        <label htmlFor="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                    </div>
-                    <Link href="">
-                        <a className="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                    </Link>
+                    <input onChange={(e) => setLoginData({email: loginData.email, password: e.target.value})} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="" />
                 </div>
                 <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
                 <div className="text-sm font-medium text-gray-500 dark:text-gray-300">
                     Not registered?
                     <Link href="/auth/register">
-                        <a className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
+                        <a className="text-blue-700 hover:underline dark:text-blue-500"> Create account</a>
                     </Link>
                 </div>
             </form>
